e088cac1f214beaa18b8019b8310daaa0b6adb14
maeng-gun <sarangiason@naver.com>
2024-01-21 22:32:41 +0900

    8) autoinvest 클래스 중간작업

--- UntitledR.R
+++ UntitledR.R
@@ -1,27 +1,35 @@
 library(R6)
 library(glue)
+library(httr)
+library(jsonlite)
 
 get_config <- function(){
   yaml::read_yaml(file = 'config.yaml', 
                   readLines.warn = F)
 }
 
+jbody <- function(body){
+  toJSON(as.list(body), auto_unbox = T)
+}
 
+#AutoInvest 클래스 정의====
 AutoInvest <- R6Class(
+  
   classname = 'AutoInvest',
   public=list(
-    # 속성 설정
-    APP_KEY=NULL, APP_SECRET=NULL, ACCT=NULL, 
+    
+    # 속성 선언 ====
+    token_tmp=NULL, APP_KEY=NULL, APP_SECRET=NULL, ACCT=NULL, 
     URL_BASE=NULL, MY_AGENT=NULL, base_headers=NULL,
     token_headers=NULL,
     
-    # 속성 초기화
-    initialize=function(account="my"){
+    # 속성 초기화 ====
+    initialize = function(account="my"){
       cfg <- get_config()
-      token_tmp <- paste0("KIS", account)
+      self$token_tmp <- paste0("KIS", account)
       
-      if (!file.exists(token_tmp)) {
-        file.create(token_tmp)
+      if (!file.exists(self$token_tmp)) {
+        file.create(self$token_tmp)
       }
       
       self$APP_KEY <- cfg[[paste0(account, '_app')]]
@@ -29,42 +37,98 @@ AutoInvest <- R6Class(
       self$ACCT <- cfg[[paste0(account, '_acct')]]
       self$URL_BASE <- cfg$prod
       self$MY_AGENT <- cfg$agent
-      self$base_headers <- list(
+      self$base_headers <- c(
         "Content-Type" = "application/json",
         "Accept" = "text/plain",
         "charset" = "UTF-8",
         'User-Agent' = self$MY_AGENT
       )
-      self$token_headers <- list(
-        # "authorization" = paste0("Bearer ", self$auth()),
+      self$token_headers <- c(
+        self$base_headers,
+        "authorization" = paste0("Bearer ", self$auth()),
         "appkey" = self$APP_KEY,
-        "appsecret" = self$APP_SECRET,
-        self$base_headers
+        "appsecret" = self$APP_SECRET
       )
+    },
+    
+    #메서드(1) - 토큰 저장하기 ====
+    save_token = function(my_token, my_expired) {
+      valid_date <- 
+        as.POSIXct(my_expired, format='%Y-%m-%d %H:%M:%S', tz='UTC')
+      writeLines(c(paste('token:', my_token),
+                   paste('valid-date:', 
+                         format(valid_date, '%Y-%m-%d %H:%M:%S'))),
+                 self$token_tmp)
+    },
+    
+    #메서드(2) - 토큰 불러오기 ====
+    read_token = function() {
+      tryCatch({
+        # 토큰이 저장된 파일 읽기
+        tkg_tmp <- yaml::read_yaml(self$token_tmp, fileEncoding = 'UTF-8')
+        
+        # 토큰 만료 일,시간
+        exp_dt <- as.POSIXct(tkg_tmp$`valid-date`)
+        # 현재일자,시간
+        now_dt <- Sys.time()
+        
+        # 저장된 토큰 만료일자 체크 (만료일시 > 현재일시 인경우 보관 토큰 리턴)
+        if (exp_dt > now_dt) {
+          return(tkg_tmp$token)
+        } else {
+          cat('Need new token: ', tkg_tmp$`valid-date`, '\n')
+          return(NULL)
+        }
+      }, error = function(e) {
+        return(NULL)
+      })
+    },
+    
+    #메서드(3) - 인증하기 ====
+    auth = function() {
+      body <- c("grant_type" = "client_credentials",
+                "appkey" = self$APP_KEY,
+                "appsecret" = self$APP_SECRET)
       
+      # 기존 발급된 토큰이 있는지 확인
+      saved_token <- self$read_token()
+      
+      if (is.null(saved_token)) {  # 기존 발급 토큰 확인이 안되면 발급 처리
+        path <- "oauth2/tokenP"
+        URL <- glue("{self$URL_BASE}/{path}")
+        res <- POST(URL, body = jbody(body), encode = "json", 
+                    add_headers(.headers = self$base_headers))
+        
+        if (res$status_code == 200) {  # 토큰 정상 발급
+          my_token <- content(res)$access_token
+          my_expired <- content(res)$access_token_token_expired
+          self$save_token(my_token, my_expired)  # 새로 발급 받은 토큰 저장
+        } else {
+          cat("Get Authentification token fail!\nYou have to restart your app!!!\n")
+          return(NULL)
+        }
+      } else {
+        my_token <- saved_token  # 기존 발급 토큰 확인되어 기존 토큰 사용
+      }
+      
+      return(my_token)
+    },
+    
+    #메서드(4) - 해시키 얻기 ====
+    hashkey = function(datas) {
+      path <- "uapi/hashkey"
+      URL <- glue("{self$URL_BASE}/{path}")
+      headers <- c(self$base_headers, 
+                   appKey = self$APP_KEY, 
+                   appSecret = self$APP_SECRET)
+      res <- POST(URL, body = jbody(datas), encode = "json",
+                  add_headers(.headers = headers))
+      hashkey <- content(res)$HASH
+      
+      return(hashkey)
     }
   )
 )
 
-
-cfg <- get_config()
-
-cfg['prod']
-
-self <- AutoInvest$new()
-
-self$base_headers
-
-self$cfg
-
-ex1 <- exam$new()
-ex1$b
-
-exam$b
-
-# 선언을 생략한 R6 클래스 정의
-MyClass <- R6Class(
-  class"MyClass",
-  public = list(
-    a=1,
-    # 메서드
+ai_my <- AutoInvest$new('my')
+ai_bool <- AutoInvest$new('boolio')
