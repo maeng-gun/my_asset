64ed486cfb4f28f69df8efa744d057f8fdd82b4b
maeng-gun <sarangiason@naver.com>
2024-01-24 23:50:29 +0900

    AutoInvest 클래스 완성

--- UntitledR.R
+++ UntitledR.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(R6)
 library(glue)
 library(httr)
@@ -123,9 +124,128 @@ AutoInvest <- R6Class(
                    appSecret = self$APP_SECRET)
       res <- POST(URL, body = jbody(datas), encode = "json",
                   add_headers(.headers = headers))
-      hashkey <- content(res)$HASH
       
-      return(hashkey)
+      content(res)$HASH
+    },
+    
+    #메서드(5) - 자산별 잔고 ====
+    inquire_account_balance = function() {
+      path <- "uapi/domestic-stock/v1/trading/inquire-account-balance"
+      URL <- glue("{self$URL_BASE}/{path}")
+      
+      data <- list(
+        CANO = self$ACCT,
+        ACNT_PRDT_CD = "01",
+        INQR_DVSN_1 = "",
+        BSPR_BF_DT_APLY_YN = ""
+      )
+      
+      headers <- c(self$token_headers, "tr_id" = "CTRP6548R",
+                   "custtype" = "P")
+      res <- 
+        GET(URL, query = data, add_headers(.headers = headers)) |> 
+        content("text", encoding = 'UTF-8') |> 
+        fromJSON()
+      
+      asset <- c("주식", "펀드_MMW", "채권", "ELS_DLS", "WRAP",
+                 "신탁_퇴직연금_외화신탁", "RP_발행어음", "해외주식",
+                 "해외채권", "금현물", "CD_CP", "단기사채",
+                 "타사상품", "외화단기사채", "외화ELS_DLS", "외화",
+                 "예수금+CMA", "청약자예수", "합계")
+      
+      res$output1 |> 
+        tibble() |> 
+        rename_with(~c('매입금액', '평가금액', '평가손익', 
+                       '신용대출', '순자산', '비중')) |> 
+        mutate(자산구분 = asset, .before = 1) |> 
+        mutate(across(매입금액:비중, as.numeric)) |> 
+        filter(비중!=0)
+    },
+    
+    #메서드(6) - 국내주식 잔고 ====
+    inquire_balance = function() {
+      path <- "/uapi/domestic-stock/v1/trading/inquire-balance"
+      URL <- glue("{self$URL_BASE}/{path}")
+      
+      data <- list(
+        CANO = self$ACCT,
+        ACNT_PRDT_CD = "01",
+        AFHR_FLPR_YN = "N",
+        OFL_YN = "N",
+        INQR_DVSN = "01",
+        UNPR_DVSN = "01",
+        FUND_STTL_ICLD_YN = "N",
+        FNCG_AMT_AUTO_RDPT_YN = "N",
+        PRCS_DVSN = "01",
+        CTX_AREA_FK100 = "",
+        CTX_AREA_NK100 = ""
+      )
+      
+      headers <- c(self$token_headers, 
+                   "tr_id" = "TTTC8434R", 
+                   "custtype" = "P")
+      
+      res <- GET(URL, query = data,
+                 add_headers(.headers = headers)) |> 
+        content("text", encoding = 'UTF-8') |> 
+        fromJSON()
+      
+      res$output1 |> 
+        tibble() |> 
+        select(pdno, prdt_name, evlu_amt) |>
+        rename_with(~c('종목코드', '상품명', '평가금액'))
+
+    },
+    
+    #메서드(7) - 해외주식 잔고 ====
+    inquire_balance_ovs = function(cur = 'USD') {
+      path <- "uapi/overseas-stock/v1/trading/inquire-balance"
+      URL <- glue("{self$URL_BASE}/{path}")
+      
+      exc <- list(USD = "NASD", JPY = "TKSE")
+      data <- list(
+        CANO = self$ACCT,
+        ACNT_PRDT_CD = "01",
+        OVRS_EXCG_CD = exc[[cur]],
+        TR_CRCY_CD = cur,
+        CTX_AREA_FK200 = "",
+        CTX_AREA_NK200 = ""
+      )
+      
+      headers <- c(self$token_headers, "tr_id" = "TTTS3012R", "custtype" = "P")
+      
+      res <- GET(URL, query = data,
+                 add_headers(.headers = headers)) |> 
+        content("text", encoding = 'UTF-8') |> 
+        fromJSON()
+      
+      res$output1 |> 
+        tibble() |> 
+        select(ovrs_pdno, ovrs_item_name, 
+               ovrs_stck_evlu_amt) |> 
+        rename_with(~c('종목코드', '상품명', '평가금액'))
+    },
+    
+    #메서드(8) - 개별종목 현재가 ====
+    get_current_price = function(sym_cd) {
+      path <- "/uapi/domestic-stock/v1/quotations/inquire-price"
+      URL <- glue("{self$URL_BASE}/{path}")
+      
+      data <- list(
+        FID_COND_MRKT_DIV_CODE = "J",
+        FID_INPUT_ISCD = sym_cd
+      )
+      
+      headers <- c(self$token_headers, 
+                   "tr_id" = "FHKST01010100", 
+                   "custtype" = "P")
+      
+      res <- GET(URL, query = data,
+                 add_headers(.headers = headers)) |> 
+        content("text", encoding = 'UTF-8') |> 
+        fromJSON()
+      
+      res$output
     }
   )
 )
