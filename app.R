302a1f66f817ead5f4454d4c6dee249d9bf43a68
maeng-gun <sarangiason@naver.com>
2024-02-14 00:22:22 +0900

    .

--- app.R
+++ app.R
@@ -9,353 +9,353 @@ import::from(shinyjs, useShinyjs, extendShinyjs, js)
 
 # 1. 대쉬보드 헤더####
 header <- dashboardHeader(
-    title = dashboardBrand(
-        title = div("포트폴리오 관리", align = 'center'),
-        color = "info"
-    )
+  title = dashboardBrand(
+    title = div("포트폴리오 관리", align = 'center'),
+    color = "info"
+  )
 )
 
 # 2. 대쉬보드 사이드바(메뉴)####
 sidebar <- dashboardSidebar(
-    sidebarMenu(
-        id = 'menu_tabs',
-        sidebarHeader("경제지표"),
-        menuItem(
-            text = "한국은행 지표선정",
-            icon = icon(name = "hand-pointer"),
-            tabName = "ecos_stat"
-        ),
-        menuItem(
-          text = "포트폴리오 운용현황",
-          icon = icon(name = "sack-dollar"),
-          tabName = "pf_bs_pl"
-        )
+  sidebarMenu(
+    id = 'menu_tabs',
+    sidebarHeader("경제지표"),
+    menuItem(
+      text = "한국은행 지표선정",
+      icon = icon(name = "hand-pointer"),
+      tabName = "ecos_stat"
     ),
-    br(),
-    actionButton('close_win',
-                 label = '프로그램 종료',
-                 width='90%',
-                 status='primary')
+    menuItem(
+      text = "포트폴리오 운용현황",
+      icon = icon(name = "sack-dollar"),
+      tabName = "pf_bs_pl"
+    )
+  ),
+  br(),
+  actionButton('close_win',
+               label = '프로그램 종료',
+               width='90%',
+               status='primary')
 )
 
 # 3. 대쉬보드 본문####
 body <- dashboardBody(
-    useShinyjs(),
-    extendShinyjs(text = "shinyjs.closeWindow = function() { window.close(); }", 
-                  functions = c("closeWindow")),
-    useSweetAlert(),
-    useWaiter(),
-    tabItems(
-        #_1) 한국은행 지표선정====
-        tabItem(
-            tabName = "ecos_stat",
-            tabBox(
-                width=12,
-                status='primary',
-                type='tabs',
-                # 탭 - 선정 아이템====
-                tabPanel(
-                    title = "선정 아이템",
-                    icon=icon('square-check'),
-                    tableOutput('selected_item')
-                ),
-                # 탭 - 통계표 조회====
-                tabPanel(
-                    title = "통계표 조회",
-                    icon=icon('table-list'),
-                    fluidRow(
-                        column(3,textInput('name','검색어')),
-                        column(9,tableOutput('ecos_stat_tables'))
-                    )
-                ),
-                # 탭 - 아이템 추가====
-                tabPanel(
-                    title = "아이템 추가",
-                    icon=icon('square-plus'),
-                    fluidRow(
-                        column(
-                            width=3,
-                            selectizeInput('name_in','통계표이름','전체'),
-                            selectizeInput('code_in','통계표코드','전체'),
-                            selectizeInput('item_in','아이템이름','전체'),
-                            selectizeInput('cyl_in','데이터주기','전체'),
-                            br(),
-                            textInput('ecos_name',"아이템명 설정", 
-                                      width='100%'),
-                            actionButton('add_item','아이템 추가',
-                                         icon=icon('square-plus'),
-                                         width='100%',
-                                         status='primary')
-                        ),
-                        column(9, tableOutput('ecos_item_tables'))
-                    )
-                )
-            )
+  useShinyjs(),
+  extendShinyjs(text = "shinyjs.closeWindow = function() { window.close(); }", 
+                functions = c("closeWindow")),
+  useSweetAlert(),
+  useWaiter(),
+  tabItems(
+    #_1) 한국은행 지표선정====
+    tabItem(
+      tabName = "ecos_stat",
+      tabBox(
+        width=12,
+        status='primary',
+        type='tabs',
+        # 탭 - 선정 아이템====
+        tabPanel(
+          title = "선정 아이템",
+          icon=icon('square-check'),
+          tableOutput('selected_item')
         ),
-        #_2) 포트폴리오 운용현황====
-        tabItem(
-          tabName = 'pf_bs_pl',
-          actionButton('kis','한투접속'),
-          br(),
-          br(),
-          tabBox(
-            id='pf_box1',
-            width=12,
-            status='primary',
-            type='tabs',
-            
-            #__a. 투자현황====
-            tabPanel(
-              title="투자현황",
-              h5("1. 자산군별 배분현황"),
+        # 탭 - 통계표 조회====
+        tabPanel(
+          title = "통계표 조회",
+          icon=icon('table-list'),
+          fluidRow(
+            column(3,textInput('name','검색어')),
+            column(9,tableOutput('ecos_stat_tables'))
+          )
+        ),
+        # 탭 - 아이템 추가====
+        tabPanel(
+          title = "아이템 추가",
+          icon=icon('square-plus'),
+          fluidRow(
+            column(
+              width=3,
+              selectizeInput('name_in','통계표이름','전체'),
+              selectizeInput('code_in','통계표코드','전체'),
+              selectizeInput('item_in','아이템이름','전체'),
+              selectizeInput('cyl_in','데이터주기','전체'),
               br(),
-              fluidRow(
-                column(
-                  width = 4,
-                  uiOutput("allo0")
-                ),
-                column(
-                  width = 8,
-                  uiOutput("allo1")
-                )
-              ),
-              h5("2. 통화별 배분현황"),
-              br(),
-              fluidRow(
-                column(
-                  width = 4,
-                  uiOutput("allo2")
-                ),
-                column(
-                  width = 8,
-                  uiOutput("allo3")
-                )
-              ),
-              h5("3. 불리오 배분현황"),
-              br(),
-              fluidRow(
-                column(
-                  width = 4,
-                  uiOutput("allo5")
-                ),
-                column(
-                  width = 8,
-                  uiOutput("allo4")
-                )
-              )
+              textInput('ecos_name',"아이템명 설정", 
+                        width='100%'),
+              actionButton('add_item','아이템 추가',
+                           icon=icon('square-plus'),
+                           width='100%',
+                           status='primary')
             ),
-            #__b. 손익현황====
-            tabPanel(
-              title="손익현황",
-              h5("1. 자산군별 손익현황"),
-              br(),
-              fluidRow(
-                uiOutput("class_ret")
-              ),
-              br(),
-              h5("2. 개별자산 손익현황"),
-              br(),
-              fluidRow(
-                uiOutput("bs_pl_mkt")
-              )
-            )
+            column(9, tableOutput('ecos_item_tables'))
           )
         )
+      )
+    ),
+    #_2) 포트폴리오 운용현황====
+    tabItem(
+      tabName = 'pf_bs_pl',
+      actionButton('kis','한투접속'),
+      br(),
+      br(),
+      tabBox(
+        id='pf_box1',
+        width=12,
+        status='primary',
+        type='tabs',
+        
+        #__a. 투자현황====
+        tabPanel(
+          title="투자현황",
+          h5("1. 자산군별 배분현황"),
+          br(),
+          fluidRow(
+            column(
+              width = 4,
+              uiOutput("allo0")
+            ),
+            column(
+              width = 8,
+              uiOutput("allo1")
+            )
+          ),
+          h5("2. 통화별 배분현황"),
+          br(),
+          fluidRow(
+            column(
+              width = 4,
+              uiOutput("allo2")
+            ),
+            column(
+              width = 8,
+              uiOutput("allo3")
+            )
+          ),
+          h5("3. 불리오 배분현황"),
+          br(),
+          fluidRow(
+            column(
+              width = 4,
+              uiOutput("allo5")
+            ),
+            column(
+              width = 8,
+              uiOutput("allo4")
+            )
+          )
+        ),
+        #__b. 손익현황====
+        tabPanel(
+          title="손익현황",
+          h5("1. 자산군별 손익현황"),
+          br(),
+          fluidRow(
+            uiOutput("class_ret")
+          ),
+          br(),
+          h5("2. 개별자산 손익현황"),
+          br(),
+          fluidRow(
+            uiOutput("bs_pl_mkt")
+          )
+        )
+      )
     )
+  )
 )
 
 #바닥글
 footer <- dashboardFooter(
-    right = "developed by H.M. Choi"
+  right = "developed by H.M. Choi"
 )
 
 
 ui <- dashboardPage(
-    header, sidebar, body, footer=footer
+  header, sidebar, body, footer=footer
 )
 
 
 # <Server> ====
-    
+
 server <- function(input, output, session) {
+  
+  
+  source("functions.R", echo=F)
+  
+  ec = Ecos$new()
+  
+  rv <-  reactiveValues(
+    name_in='전체',
+    code='전체', df=NULL, df2=NULL, 
+    df3=NULL, df4=NULL, df5=NULL,
+    df_s=ec$read_items(),
+    df_d=NULL
+  )
+  
+  #통계표 조회====
+  observeEvent(input$name,{
+    rv$df <- ec$find_stat(input$name)
+    updateSelectizeInput(session, 'name_in', 
+                         choices = c('전체',rv$df$stat_name),
+                         selected = '전체')
     
+  })
+  
+  #아이템 추가==== 
+  
+  # * 통계표 이름==== 
+  observeEvent(input$name_in,{
     
-    source("functions.R", echo=F)
+    rv$name_in <- input$name_in
     
-    ec = Ecos$new()
-
-    rv <-  reactiveValues(
-        name_in='전체',
-        code='전체', df=NULL, df2=NULL, 
-        df3=NULL, df4=NULL, df5=NULL,
-        df_s=ec$read_items(),
-        df_d=NULL
-        )
+    updateSelectizeInput(session, 'name_in',
+                         choices = c('전체', rv$df$stat_name),
+                         selected = rv$name_in)
     
-    #통계표 조회====
-    observeEvent(input$name,{
-        rv$df <- ec$find_stat(input$name)
-        updateSelectizeInput(session, 'name_in', 
-                             choices = c('전체',rv$df$stat_name),
-                             selected = '전체')
-        
-    })
-    
-    #아이템 추가==== 
-    
-    # * 통계표 이름==== 
-    observeEvent(input$name_in,{
-
-        rv$name_in <- input$name_in
-
-        updateSelectizeInput(session, 'name_in',
-                             choices = c('전체', rv$df$stat_name),
-                             selected = rv$name_in)
-        
-        if(rv$name_in=='전체'){
-            code <- '전체'
-            rv$code <- '전체'
-        }
-        else {
-            rv$df_d <- rv$df |> filter(stat_name==rv$name_in)
-            code <- rv$df_d$stat_code
-            
-            if(length(code)==1){rv$code <- code}
-            else {rv$code <- code[1]}
-            
-        }
-        updateSelectizeInput(session, 'code_in', choices = code,
-                             selected = rv$code)
-    })
-    
-    # * 통계표 코드==== 
-    observeEvent(input$code_in,{
-        tryCatch({
-            rv$df2 <- ec$find_items(input$code_in)
-        }, error = function(e) {
-            rv$df2 <- ec$find_items('전체')
-        })
+    if(rv$name_in=='전체'){
+      code <- '전체'
+      rv$code <- '전체'
+    }
+    else {
+      rv$df_d <- rv$df |> filter(stat_name==rv$name_in)
+      code <- rv$df_d$stat_code
       
-        rv$df5 <- rv$df2
-
-        updateSelectizeInput(session, 'item_in',
-                             choices = c('전체', unique(rv$df2$item_name)),
-                             selected = '전체')
-        updateSelectizeInput(session, 'cyl_in',
-                             choices = c('전체', unique(rv$df2$cycle)),
-                             selected = '전체')
-    })
-
-    # * 아이템이름==== 
-    observeEvent(input$item_in,{
-        if(is.null(input$item_in)||input$item_in=='전체'){
-            rv$df3 <- rv$df2
-        }
-        else rv$df3 <- rv$df2 |> filter(item_name==input$item_in)
-
-        rv$df5 <- rv$df3
-        
-        updateSelectizeInput(session, 'item_in',
-                             choices = c('전체', unique(rv$df2$item_name)),
-                             selected = input$item_in)
-        
-        updateSelectizeInput(session, 'cyl_in',
-                             choices = c('전체', unique(rv$df2$cycle)),
-                             selected = '전체')        
+      if(length(code)==1){rv$code <- code}
+      else {rv$code <- code[1]}
+      
+    }
+    updateSelectizeInput(session, 'code_in', choices = code,
+                         selected = rv$code)
+  })
+  
+  # * 통계표 코드==== 
+  observeEvent(input$code_in,{
+    tryCatch({
+      rv$df2 <- ec$find_items(input$code_in)
+    }, error = function(e) {
+      rv$df2 <- ec$find_items('전체')
     })
     
-    # * 데이터주기====    
-    observeEvent(input$cyl_in,{
-        if(is.null(input$cyl_in)||input$cyl_in=='전체'){
-            rv$df4 <- rv$df3
-        }
-        else rv$df4 <- rv$df3 |> filter(cycle==input$cyl_in)
-
-        rv$df5 <- rv$df4
-                
-        updateSelectizeInput(session, 'cyl_in',
-                             choices = c('전체', unique(rv$df3$cycle)),
-                             selected = input$cyl_in)
-    })
+    rv$df5 <- rv$df2
     
-    # * 아이템 추가====    
-    observeEvent(input$add_item,{
-        ec$save_items(rv$df5, input$ecos_name)
-        sendSweetAlert(title="추가하였습니다!", type='success')
-        rv$df_s <- ec$read_items()
-    })
+    updateSelectizeInput(session, 'item_in',
+                         choices = c('전체', unique(rv$df2$item_name)),
+                         selected = '전체')
+    updateSelectizeInput(session, 'cyl_in',
+                         choices = c('전체', unique(rv$df2$cycle)),
+                         selected = '전체')
+  })
+  
+  # * 아이템이름==== 
+  observeEvent(input$item_in,{
+    if(is.null(input$item_in)||input$item_in=='전체'){
+      rv$df3 <- rv$df2
+    }
+    else rv$df3 <- rv$df2 |> filter(item_name==input$item_in)
     
-    observe({
-        output$selected_item <- renderTable(rv$df_s)
-        output$ecos_stat_tables <- renderTable(rv$df)
-        output$ecos_item_tables <- renderTable(rv$df5)
-    })
+    rv$df5 <- rv$df3
     
-    #자산배분 현황 확인====
-    observeEvent(input$kis,{
-      w1 <- Waiter$new(
-        id='pf_box1',
-        html = tagList(spin_loader(), "로딩중..."),
-        color = transparent(.5))
-      
-      w1$show()
-
-      ma = MyAssets$new()
-      
-      w1$hide()
-      
-      render_allo <- function(df){
-        renderUI({
-          df |> flextable() |> 
-            theme_vanilla() |> 
-            merge_v(j=1:2) |>
-            set_table_properties(layout='autofit',width=0.9) |> 
-            htmltools_value()
-        })
-      }
-      
-      output$allo0 <- render_allo(ma$allo0)
-      output$allo1 <- render_allo(ma$allo1)
-      output$allo2 <- render_allo(ma$allo2)
-      output$allo3 <- render_allo(ma$allo3)
-      output$allo4 <- render_allo(ma$allo4)
-      output$allo5 <- render_allo(ma$allo5)
-      
-      output$class_ret <- renderUI({
-        ma$ret |> 
-          select(1:3,평가금액,실현손익, 평가손익증감, 평가손익, 
-                 실현수익률:평가수익률) |> 
-          flextable() |> 
+    updateSelectizeInput(session, 'item_in',
+                         choices = c('전체', unique(rv$df2$item_name)),
+                         selected = input$item_in)
+    
+    updateSelectizeInput(session, 'cyl_in',
+                         choices = c('전체', unique(rv$df2$cycle)),
+                         selected = '전체')        
+  })
+  
+  # * 데이터주기====    
+  observeEvent(input$cyl_in,{
+    if(is.null(input$cyl_in)||input$cyl_in=='전체'){
+      rv$df4 <- rv$df3
+    }
+    else rv$df4 <- rv$df3 |> filter(cycle==input$cyl_in)
+    
+    rv$df5 <- rv$df4
+    
+    updateSelectizeInput(session, 'cyl_in',
+                         choices = c('전체', unique(rv$df3$cycle)),
+                         selected = input$cyl_in)
+  })
+  
+  # * 아이템 추가====    
+  observeEvent(input$add_item,{
+    ec$save_items(rv$df5, input$ecos_name)
+    sendSweetAlert(title="추가하였습니다!", type='success')
+    rv$df_s <- ec$read_items()
+  })
+  
+  observe({
+    output$selected_item <- renderTable(rv$df_s)
+    output$ecos_stat_tables <- renderTable(rv$df)
+    output$ecos_item_tables <- renderTable(rv$df5)
+  })
+  
+  #자산배분 현황 확인====
+  observeEvent(input$kis,{
+    w1 <- Waiter$new(
+      id='pf_box1',
+      html = tagList(spin_loader(), "로딩중..."),
+      color = transparent(.5))
+    
+    w1$show()
+    
+    ma = MyAssets$new()
+    
+    w1$hide()
+    
+    render_allo <- function(df){
+      renderUI({
+        df |> flextable() |> 
           theme_vanilla() |> 
           merge_v(j=1:2) |>
-          set_table_properties(layout='autofit') |>
-          colformat_double(j=4:7, digits = 0) |>
-          colformat_double(j=8:10, digits = 2) |> 
+          set_table_properties(layout='autofit',width=0.9) |> 
           htmltools_value()
       })
-      
-      output$bs_pl_mkt <-renderUI({
-        ma$bs_pl_mkt |> 
-          select(통화, 자산군, 세부자산군, 종목명,
-                 보유수량,평가금액, 실현손익,평가손익증감, 평가손익,
-                 실현수익률,운용수익률,평가수익률) |>
-          arrange(통화,자산군,세부자산군) |> 
-          flextable() |> 
-          theme_vanilla() |> 
-          merge_v(j=1:3) |>
-          set_table_properties(layout='autofit') |>
-          colformat_double(j=5:9, digits = 0) |>
-          colformat_double(j=10:12, digits = 2) |> 
-          htmltools_value()
-      })
-      
+    }
+    
+    output$allo0 <- render_allo(ma$allo0)
+    output$allo1 <- render_allo(ma$allo1)
+    output$allo2 <- render_allo(ma$allo2)
+    output$allo3 <- render_allo(ma$allo3)
+    output$allo4 <- render_allo(ma$allo4)
+    output$allo5 <- render_allo(ma$allo5)
+    
+    output$class_ret <- renderUI({
+      ma$ret |> 
+        select(1:3,평가금액,실현손익, 평가손익증감, 평가손익, 
+               실현수익률:평가수익률) |> 
+        flextable() |> 
+        theme_vanilla() |> 
+        merge_v(j=1:2) |>
+        set_table_properties(layout='autofit') |>
+        colformat_double(j=4:7, digits = 0) |>
+        colformat_double(j=8:10, digits = 2) |> 
+        htmltools_value()
     })
     
-    
-    observeEvent(input$close_win,{
-      js$closeWindow()
-      stopApp()
+    output$bs_pl_mkt <-renderUI({
+      ma$bs_pl_mkt |> 
+        select(통화, 자산군, 세부자산군, 종목명,
+               보유수량,평가금액, 실현손익,평가손익증감, 평가손익,
+               실현수익률,운용수익률,평가수익률) |>
+        arrange(통화,자산군,세부자산군) |> 
+        flextable() |> 
+        theme_vanilla() |> 
+        merge_v(j=1:3) |>
+        set_table_properties(layout='autofit') |>
+        colformat_double(j=5:9, digits = 0) |>
+        colformat_double(j=10:12, digits = 2) |> 
+        htmltools_value()
     })
+    
+  })
+  
+  
+  observeEvent(input$close_win,{
+    js$closeWindow()
+    stopApp()
+  })
 }
 
 shinyApp(ui = ui, server = server,
